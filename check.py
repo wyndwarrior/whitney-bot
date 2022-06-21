ad8eaec77867d679dac4998e181990d496b13123
Andrew Liu <andrew@covariant.ai>
2022-06-21 16:14:43 -0700

    use fb

--- check.py
+++ check.py
@@ -1,61 +1,69 @@
 import json
+import traceback
 import requests
-from twilio.rest import Client
 import time
 import os
+from fbchat import Client
+from fbchat.models import *
+from datetime import datetime
+import pytz
+
+tz = pytz.timezone('America/Los_Angeles') 
+client = Client(os.environ['FBEMAIL'] , os.environ["FBPASSWORD"])
+
+print("Own id: {}".format(client.uid))
+
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36',
 }
 
-
-# Your Account SID from twilio.com/console
-account_sid = os.environ["ACCOUNT_SID"]
-auth_token  = os.environ['AUTH_TOKEN'] 
-to_numbers = json.loads(os.environ['TO_NUMBERS'])
-
-client = Client(account_sid, auth_token)
-
 prev_message = None
-
 if os.path.exists("prev_message.txt"):
     with open("prev_message.txt", "r") as f:
         prev_message = f.read()
 
 while True:
 
-    all_found = []
+    try:
 
-    for name, url in [
-        ("overnight", "https://www.recreation.gov/api/permits/233260/divisions/166/availability?start_date=2022-01-01T00:00:00.000Z&end_date=2023-04-30T00:00:00.000Z&commercial_acct=false"),
-        ("day permit", "https://www.recreation.gov/api/permits/233260/divisions/406/availability?start_date=2022-01-01T00:00:00.000Z&end_date=2023-04-30T00:00:00.000Z&commercial_acct=false")]:
+        all_found = []
 
-        resp = requests.get(url=url, headers=headers)
-        data = resp.json()
+        for name, url in [
+            ("overnight", "https://www.recreation.gov/api/permits/233260/divisions/166/availability?start_date=2022-01-01T00:00:00.000Z&end_date=2023-04-30T00:00:00.000Z&commercial_acct=false"),
+            # ("day permit", "https://www.recreation.gov/api/permits/233260/divisions/406/availability?start_date=2022-01-01T00:00:00.000Z&end_date=2023-04-30T00:00:00.000Z&commercial_acct=false")
+            ]:
 
-        next_available = data['payload']['next_available_date']
-        availability = data['payload']['date_availability']
-        availability_dates = list(availability.keys())
+            resp = requests.get(url=url, headers=headers)
+            data = resp.json()
 
-        if next_available in availability_dates:
-            idx = availability_dates.index(next_available)
-            for k in availability_dates[idx:]:
-                if availability[k]['remaining'] > 0:
-                    all_found.append((f"{name}: {k}, {availability[k]['remaining']} permit(s) available"))
+            next_available = data['payload']['next_available_date']
+            availability = data['payload']['date_availability']
+            availability_dates = list(availability.keys())
 
-    message = "Whitney bot:\n" + "\n".join(all_found) + "\n\nhttps://www.recreation.gov/permits/233260 to book "
+            if next_available in availability_dates:
+                idx = availability_dates.index(next_available)
+                for k in availability_dates[idx:]:
+                    if availability[k]['remaining'] > 0:
+                        all_found.append((f"{name}: {k}, {availability[k]['remaining']} permit(s) available"))
 
-    print(message)
+        message = "Whitney bot:\n" + "\n".join(all_found) + "\n\nhttps://www.recreation.gov/permits/233260 to book\n\n"
 
-    if message != prev_message:
-        for to_number in to_numbers:
-            client.messages.create(
-                to=to_number, 
-                from_=os.environ["TWILIO_NUMBER"],
-                body=message)
+        now = datetime.now(tz)
+        now_str = now.strftime("%H:%M:%S")
 
-        prev_message = message
-        with open("prev_message.txt", "w") as f:
-            f.write(message)
+        print(message + now_str)
 
-    time.sleep(60 * 5)
+        if message != prev_message:
+            client.send(Message(text=message + now_str), thread_id=os.environ['FBTHREAD'], thread_type=ThreadType.GROUP)
+
+            prev_message = message
+            with open("prev_message.txt", "w") as f:
+                f.write(message)
+    except:
+        try:
+            client.send(Message(text=traceback.format_exc()), thread_id=os.environ['FBTHREAD'], thread_type=ThreadType.GROUP)
+        except:
+            pass
+
+    time.sleep(60)
--- half_dome.py
+++ half_dome.py
@@ -1,21 +1,22 @@
 import json
+import traceback
 import requests
-from twilio.rest import Client
 import time
 import os
+
+from fbchat import Client
+from fbchat.models import *
+from datetime import datetime
+import pytz
+
+tz = pytz.timezone('America/Los_Angeles') 
+client = Client(os.environ['FBEMAIL'] , os.environ["FBPASSWORD"])
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36',
 }
 
 permit_key = '44585917'
-
-# Your Account SID from twilio.com/console
-account_sid = os.environ["ACCOUNT_SID"]
-auth_token  = os.environ['AUTH_TOKEN'] 
-to_numbers = json.loads(os.environ['TO_NUMBERS_HD'])
-
-client = Client(account_sid, auth_token)
 
 prev_message = None
 
@@ -24,30 +25,41 @@ if os.path.exists("prev_message_hd.txt"):
         prev_message = f.read()
 
 while True:
+    now = datetime.now(tz)
+    try:
+        all_found = []
 
-    all_found = []
+        for url in [
+            'https://www.recreation.gov/api/permitinyo/445859/availability?start_date=2022-07-01&end_date=2022-07-31&commercial_acct=false',
+            "https://www.recreation.gov/api/permitinyo/445859/availability?start_date=2022-06-01&end_date=2022-06-30&commercial_acct=false",
+        ]:
+            resp = requests.get(url=url, headers=headers)
+            data = resp.json()
 
-    resp = requests.get(url="https://www.recreation.gov/api/permitinyo/445859/availability?start_date=2022-06-01&end_date=2022-06-30&commercial_acct=false", headers=headers)
-    data = resp.json()
+            availability = data['payload']
+            for date, date_avail in availability.items():
+                if permit_key in date_avail and date_avail[permit_key]['remaining'] > 0:
+                    all_found.append((f"{date}: {date_avail[permit_key]['remaining']} permit(s) available"))
 
-    availability = data['payload']
-    for date, date_avail in availability.items():
-        if permit_key in date_avail and date_avail[permit_key]['remaining'] > 0:
-            all_found.append((f"{date}: {date_avail[permit_key]['remaining']} permit(s) available"))
+        message = "Half Dome bot:\n" + "\n".join(all_found) + "\n\nhttps://www.recreation.gov/permits/445859/registration/detailed-availability to book\n\n"
 
-    message = "Half Dome bot:\n" + "\n".join(all_found) + "\n\nhttps://www.recreation.gov/permits/445859/registration/detailed-availability to book "
+        now_str = now.strftime("%H:%M:%S")
 
-    print(message)
+        print(message + now_str)
 
-    if message != prev_message:
-        for to_number in to_numbers:
-            client.messages.create(
-                to=to_number, 
-                from_=os.environ["TWILIO_NUMBER"],
-                body=message)
+        if message != prev_message:
+            client.send(Message(text=message + now_str), thread_id=os.environ['FBTHREAD'], thread_type=ThreadType.GROUP)
 
-        prev_message = message
-        with open("prev_message_hd.txt", "w") as f:
-            f.write(message)
-
-    time.sleep(60 * 5)
+            prev_message = message
+            with open("prev_message.txt", "w") as f:
+                f.write(message)
+    except:
+        try:
+            client.send(Message(text=traceback.format_exc()), thread_id=os.environ['FBTHREAD'], thread_type=ThreadType.GROUP)
+        except:
+            pass
+    if abs((now.replace(hour=7, minute=0) - now).total_seconds()) < 60*30:
+        print(abs((now.replace(hour=7, minute=0) - now).total_seconds()))
+        time.sleep(10)
+    else:
+        time.sleep(60)
